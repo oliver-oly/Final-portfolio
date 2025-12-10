c8c1ca656c100fbdc4c36bbf590ceb3cdb18571f
oliver-oly <W0521046@campus.nscc.ca>
2025-12-09 21:59:44 -0400

    Add files via upload

---
+++ final-portfolio/js/main.js
@@ -0,0 +1,8 @@
+// Mobile hamburger menu
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+menuToggle.addEventListener("click", () => {
+  mainNav.classList.toggle("open");
+});
+
